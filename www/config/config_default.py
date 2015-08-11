69eb2660e93bfeb09478deef982dd6e66893f5b2
zathing <zathing@outlook.com>
2015-08-12 01:02:15 +0800

    Day15

---
+++ www/config/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- codeing: utf-8 -*-
+
+__author__ = 'zathing'
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'ZiYaTalk'
+    }
+}
--- www/wsgiapp.py
+++ www/wsgiapp.py
@@ -8,12 +8,12 @@ A WSGI application entry.
 '''
 
 import logging; logging.basicConfig(level=logging.INFO)
-import os, sys, time
-sys.path.append('..')
+import os, time
 import urls
+from datetime import datetime
 from transwarp import db
 from transwarp.web import WSGIApplication, Jinja2TemplateEngine
-from conf.config import configs
+from config.config import configs
 
 # 定义datetime_filter，输入是t，输出是unicode字符串:
 def datetime_filter(t):
@@ -39,6 +39,8 @@ template_engine = Jinja2TemplateEngine(os.path.join(os.path.dirname(os.path.absp
 template_engine.add_filter('datetime', datetime_filter)
 
 wsgi.template_engine = template_engine
+wsgi.add_interceptor(urls.user_interceptor)
+wsgi.add_interceptor(urls.manage_interceptor)
 wsgi.add_module(urls)
 
 if __name__ == '__main__':
